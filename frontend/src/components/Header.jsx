9c7a9030330ed4c8d85a477b34f28c19206f182d
Marcia Souza Mendonça <marciasouzzam@gmail.com>
2026-04-21 21:23:23 -0300

    feat: logo passa a ser clicável

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import logoImg from '../assets/logo.svg'
 import fotoUsuarioImg from '../assets/fotoUsuario.png'
 
@@ -5,13 +6,13 @@ export default function Header() {
   return (
     <header className="w-full h-20 bg-white border-b border-divisor flex items-center justify-center">
       <div className="w-full max-w-[1440px] px-[207px] flex items-center justify-between">
-        <div className="flex items-center">
+        <Link to="/" className="flex items-center cursor-pointer"> {/* extra? */}
           <img
             src={logoImg}
             alt="Logo Corp News"
             className="h-12 w-auto object-contain"
           />
-        </div>
+        </Link>
         <div className="flex items-center gap-3">
           <div className="w-10 h-10 rounded-full overflow-hidden border border-divisor">
             <img
